07def45c6974617f7cced747ced1f2a97b9f8a7e
El Che <gaodes@gmail.com>
2026-04-16 18:19:28 +0300

    refactor(devkit): split into modular structure (tools/, commands/, extensions/)
    - Remove monolithic index.ts entry point
    - Add extensions/index.ts as new entry (calls setupTools + registerUpdateCommand)
    - tools/ directory: changelog, docs, version, package-manager, ui, utils
    - commands/update.ts: /devkit command
    - prompts/: placeholder for future prompts
    - package.json: pi.extensions now points to ./extensions directory

--- extensions/pure-devkit/extensions/index.ts
+++ extensions/pure-devkit/extensions/index.ts
@@ -1,6 +1,6 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
-import { registerUpdateCommand } from "./commands/update";
-import { setupTools } from "./tools";
+import { registerUpdateCommand } from "../commands/update";
+import { setupTools } from "../tools";
 
 export default function (pi: ExtensionAPI) {
 	setupTools(pi);
